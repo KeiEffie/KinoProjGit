c705f29e0c760b4b875bdb2e4290036ba03b0260
[ki.effie@yandex.com] <ki.effie@yandex.com>
2019-04-01 01:51:54 +0300

    ResultsMethod

---
+++ KinoProject/Lottery.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace KinoProject
+{
+    class Lottery
+    {
+        //the lottery event - entering players into a lottery, 
+        //interface  - how to enter player details and ticket details etc.
+        //drawing a lottery
+        public List<Draw> DrawL;
+        public List<Ticket> TicketL;
+        public List<Player> PlayersL;
+        public List<int> WonNumbers { get; set; }
+
+
+        public Lottery()
+        {
+            DrawL == new List<Draw>();
+            TicketL = new List<Ticket>();
+            PlayersL=new List<Player>();
+            WonNumbers = new List<WonNumbers>();
+        }
+
+        public static void Results(List<Draw> drawL, List<Ticket> ticketL)
+        {
+            foreach (Ticket t in ticketL)
+            {
+            }
+            Console.WriteLine($"For Ticket Number: {ticket.ID}");            
+            for (int i=0; i < ticket.player.NumbersToPlay.Capacity; i++)
+            {
+                foreach (int j in ticket.player.NumbersToPlay)
+                {
+                    draw.WinnerList.Contains(j);
+
+                }
+                
+            }
+            
+
+        }
+        public static void Statistics(Draw draw, Ticket ticket)
+        {
+
+        }
+           
+
+        
+    }
+}
